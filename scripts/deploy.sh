8133bc0a50d902c8fb8a26f226575684b93804b6
Brian Gradin <gradinafrica@yahoo.com>
2021-01-18 16:50:03 -0800

    Fixes CI

---
+++ scripts/deploy.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+cd public
+find . \
+  -wholename '*' \
+  -printf '%p\n' \
+  -type f \
+  -exec curl -sk \
+    sftp://${SFTP_HOST}:${SFTP_PORT}/~/html/{} \
+    -u ${SFTP_USER}:${SFTP_PASSWORD} \
+    -T {} --ftp-create-dirs\;
